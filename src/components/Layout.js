7f7fc42e8cab3cc7bfbddbc1a0fab8d879a4b9b6
cohet3 <tauro.total90@gmail.com>
2023-01-26 14:15:34 +0100

    second commit

---
+++ src/components/Layout.js
@@ -0,0 +1,30 @@
+import { Component } from "react";
+
+const styles = {
+    layout: {
+        backgroundColor: '#fff',
+        color: '#A283E',
+        alignItems: 'center',
+        display: 'flex',
+        flexDirection: 'column',
+    },
+
+
+container:{
+    width: '1200px',
+  }
+}
+
+class Layout extends Component {
+    render() {
+        return (
+            <div styles={styles.layout}>
+                <div styles={styles.container}>
+                    {this.props.children}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Layout
