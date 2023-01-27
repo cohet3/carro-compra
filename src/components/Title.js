8dcf5153ce5372c86925c55d1ab3168dd476fef7
cohet3 <tauro.total90@gmail.com>
2023-01-27 21:30:41 +0100

    Tercer commit

--- src/components/Title.js
+++ src/components/Title.js
@@ -10,7 +10,7 @@ const styles = {
 class Title extends Component {
     render(){
         return (
-        <h1 style={styles.title}>Tienda</h1>
+        <h1 style={styles.title}>Frutería</h1>
         )
     }
 }
